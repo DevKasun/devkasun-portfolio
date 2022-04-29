3f3611ee55cac378ea0ef593f3ad0c4b16ccf697
Kasun Lakshitha <kasun2014@outlook.com>
2022-04-30 03:57:56 +0530

    Experience and contact page is done

--- components/Contact.js
+++ components/Contact.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
 
 function Contact() {
     return (
         <div className="card-content">
             <div className="card-subtitle">Contact</div>
             <div className="card-desc">
-                Underconstrction
+                <div className="contact-details-wrap">
+                    <div className="contact-item">
+                        <div className="icon">
+                            <Image src="/location.svg" alt="location" width="24" height="24" />
+                        </div>
+                        <div className="text">Hunuwila, Daraluwa.(NW) Sri Lanka</div>
+                    </div>
+                    <div className="contact-item">
+                        <div className="icon">
+                            <Image src="/email.svg" alt="email" width="24" height="24" />
+                        </div>
+                        <div className="text">devkasunlakshitha@gmail.com</div>
+                    </div>
+                    <div className="contact-item">
+                        <div className="icon">
+                            <Image src="/phone.svg" alt="phone number" width="24" height="24" />
+                        </div>
+                        <div className="text">+94 77 58 59 642</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- components/Experience.js
+++ components/Experience.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import TimeLine from './TimeLine';
 
 function Experience() {
     return (
         <div className="card-content">
             <div className="card-subtitle">Experience</div>
             <div className="card-desc">
-                Underconstrction
+                <TimeLine />
             </div>
         </div>
     )

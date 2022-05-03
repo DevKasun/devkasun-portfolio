aa144518fcbac2475e1a6af3b15428d08f83d751
Kasun Lakshitha <kasun2014@outlook.com>
2022-05-04 01:47:29 +0530

    SM links

--- components/About.js
+++ components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 function About() {
     return (
@@ -13,18 +14,26 @@ function About() {
                 </div>
             </div>
             <div className="card-social">
-                <a href="">
-                    <Image src="/icons/facebook.svg" alt="avatar" width="24" height="24" />
-                </a>
-                <a href="">
-                    <Image src="/icons/twitter.svg" alt="avatar" width="24" height="24" />
-                </a>
-                <a href="">
-                    <Image src="/icons/instagram.svg" alt="avatar" width="24" height="24" />
-                </a>
-                <a href="">
-                    <Image src="/icons/github.svg" alt="avatar" width="24" height="24" />
-                </a>
+                <Link href="https://twitter.com/KasunLuckshitha">
+                    <a target="_blank">
+                        <Image src="/icons/twitter.svg" alt="avatar" width="24" height="24" />
+                    </a>
+                </Link>
+                <Link href="https://www.instagram.com/devkasun">
+                    <a target="_blank">
+                        <Image src="/icons/instagram.svg" alt="avatar" width="24" height="24" />
+                    </a>
+                </Link>
+                <Link href="https://www.facebook.com/devkasunlakshitha">
+                    <a target="_blank">
+                        <Image src="/icons/facebook.svg" alt="avatar" width="24" height="24" />
+                    </a>
+                </Link>
+                <Link href="https://github.com/DevKasun">
+                    <a target="_blank">
+                        <Image src="/icons/github.svg" alt="avatar" width="24" height="24" />
+                    </a>
+                </Link>
             </div>
         </>
     )

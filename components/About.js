85c34d8b9c1122d503d9abdd9c6135e62177106f
Kasun Lakshitha <kasun2014@outlook.com>
2022-06-01 08:21:25 +0530

    escape & sign

--- components/About.js
+++ components/About.js
@@ -10,7 +10,7 @@ function About() {
                 <div className="card-desc">
                     <p>👋 Hi, I&apos;m an Enthusiastic Frontend Developer who loves to create beautiful user interfaces. I'm glad I decided to learn to program. It's taught me how to become resourceful and think outside the box. I've been able to accomplish many things I thought were impossible.</p><br />
                     <p>
-                        <strong>Skills & Tools: </strong>HTML, CSS, Javascript, Jquery, SASS, React JS, Bootstrap, Bulma, TailwindCSS, VS Code, Terminal, Gulp, Grunt, Webpack, Vite, git,  etc.
+                        <strong>Skills &amp; Tools: </strong>HTML, CSS, Javascript, Jquery, SASS, React JS, Bootstrap, Bulma, TailwindCSS, VS Code, Terminal, Gulp, Grunt, Webpack, Vite, git,  etc.
                     </p><br />
                     <p>I love to level up my skills!</p>
 

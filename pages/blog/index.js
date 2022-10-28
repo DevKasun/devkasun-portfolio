6c5655963a3888467a058a8ffbcc3d6f840cc3a7
Kasun Lakshitha <kasun2014@outlook.com>
2022-10-29 03:39:05 +0530

    target blank to the URL

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 import Breadcrumb from '../../components/Breadcrumb/Breadcrumb';
 import SubLayout from '../../layout/SubLayout';
 import classes from './index.module.scss';
@@ -12,7 +13,7 @@ const index = () => {
                     <div className={classes.contentWrapper}>
                         <h3>My Blog website: CSS-BLOK</h3>
                         <p>As a front-end web developer, I have more than 6 years of experience. My expertise in front-end web development would be useful to others. Because of that, I decided to start a new blog site to share my knowledge. Hopefully, these articles will help people who want to make the web more colorful.</p>
-                        <p>My blog website link: <a href="https://css-blok.com">CSS-BLOK</a></p>
+                        <p>My blog website link: <Link href="https://css-blok.com"><a target="_blank">CSS-BLOK</a></Link></p>
                     </div>
                 </div>
                 <div className={classes.blogImage}>

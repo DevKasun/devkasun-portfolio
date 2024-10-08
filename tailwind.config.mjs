e943934c4607927c65d0413dfe154a1b0883c98d
Kasun Lakshitha <kasun2014@outlook.com>
2024-10-09 00:31:25 +0530

    Page main layout changed. Icons added theme colors changed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		eextend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-				'home-hero-bg-image': "url('/images/profile-pic.png')",
-			},
-		},
-		colors: {
-			'kwhite': '#FFFFFF',
-			'kgray': '#525252',
-			'kblack': '#020202',
-		}
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    eextend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "home-hero-bg-image": "url('/images/profile-pic.png')",
+      },
+    },
+    colors: {
+      kwhite: "#e5e7de",
+      kgreen: "#0b3536",
+      kred: "#f54123",
+    },
+  },
+  plugins: [],
+};

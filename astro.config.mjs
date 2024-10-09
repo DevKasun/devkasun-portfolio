59b7c4d70a045c4aa7783311bb4b2dd011bad9c0
Kasun Lakshitha <kasun2014@outlook.com>
2024-10-09 12:58:38 +0530

    installed the sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://devkasun.me',
+  site: "https://devkasun.me",
   integrations: [mdx(), sitemap(), tailwind()],
-  adapter: vercel()
+  adapter: vercel(),
+  image: {
+    service: {
+      entrypoint: "astro/assets/services/sharp",
+      config: {
+        limitInputPixels: 268435456, // 16384 x 16384 pixels (adjust if needed)
+      },
+    },
+  },
 });

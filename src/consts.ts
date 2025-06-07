c2598d3aa94cb7f5c049347281774566bbd4d6ef
Kasun Lakshitha <kasun2014@outlook.com>
2025-06-07 22:57:49 +0530

    About page updated

--- src/consts.ts
+++ src/consts.ts
@@ -1,72 +1,86 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Astro Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
 
-export const HOME_SITE_TITLE = 'DevKasun - Home Page';
-export const HOME_SITE_DESCRIPTION = 'Welcome to my website!';
+export const HOME_SITE_TITLE = "DevKasun - Home Page";
+export const HOME_SITE_DESCRIPTION = "Welcome to my website!";
 
-export const WORK_SITE_TITLE = 'DevKasun - Projects Page';
-export const WORK_SITE_DESCRIPTION = 'My projects';
+export const WORK_SITE_TITLE = "DevKasun - Projects Page";
+export const WORK_SITE_DESCRIPTION = "My projects";
 
-export const ABOUT_SITE_TITLE = 'DevKasun - About Page';
-export const ABOUT_SITE_DESCRIPTION = 'Brief introduction about myself.';
+export const ABOUT_SITE_TITLE = "DevKasun - About Page";
+export const ABOUT_SITE_DESCRIPTION = "Brief introduction about myself.";
 
-export const CONTACT_SITE_TITLE = 'DevKasun - Contact Page';
-export const CONTACT_SITE_DESCRIPTION = 'Contact me!';
+export const CONTACT_SITE_TITLE = "DevKasun - Contact Page";
+export const CONTACT_SITE_DESCRIPTION = "Contact me!";
 
-export const BLOG_SITE_TITLE = 'DevKasun - Blog page';
-export const BLOG_SITE_DESCRIPTION = 'Read me blog articles';
+export const BLOG_SITE_TITLE = "DevKasun - Blog page";
+export const BLOG_SITE_DESCRIPTION = "Read me blog articles";
 
 export const skills = [
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'jQuery',
-    'SASS',
-    'React JS',
-    'Next JS',
-    'Bootstrap',
-    'Bulma',
-    'Tailwind CSS',
-    'VS Code',
-    'Terminal',
-    'Gulp',
-    'Grunt',
-    'Webpack',
-    'Vite',
-    'Git',
-    'WordPress'
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "jQuery",
+  "SASS",
+  "React JS",
+  "Next JS",
+  "Bootstrap",
+  "Bulma",
+  "Tailwind CSS",
+  "VS Code",
+  "Terminal",
+  "Gulp",
+  "Grunt",
+  "Webpack",
+  "Vite",
+  "Git",
+  "WordPress",
 ];
 
 export const sevenSignsSkills = [
-    'Front-End Development',
-    'WordPress',
-    'Wordpress Development',
-    'PSD to Wordpress',
-    'Linux',
-    'jQuery',
-    'SQL',
-    'MySQL',
-    'Bootstrap',
-    'SASS',
-    'Vue.js'
+  "Front-End Development",
+  "WordPress",
+  "Wordpress Development",
+  "PSD to Wordpress",
+  "Linux",
+  "jQuery",
+  "SQL",
+  "MySQL",
+  "Bootstrap",
+  "SASS",
+  "Vue.js",
 ];
 
 export const RhinoPartnersSkills = [
-    'Next.js',
-    'React.js',
-    'Front-End Development',
-    'Tailwind CSS',
-    'Redux.js',
-    'WordPress',
-    'Wordpress Development',
-    'Laravel',
-    'Linux',
-    'jQuery',
-    'SQL',
-    'MySQL',
-    'Bootstrap',
-    'SASS'
+  "Next.js",
+  "React.js",
+  "Front-End Development",
+  "Tailwind CSS",
+  "Redux.js",
+  "WordPress",
+  "Wordpress Development",
+  "Laravel",
+  "Linux",
+  "jQuery",
+  "SQL",
+  "MySQL",
+  "Bootstrap",
+  "SASS",
+];
+
+export const FreelanceSkills = [
+  "React.js",
+  "Next.js",
+  "Front-End Development",
+  "Tailwind CSS",
+  "Mantine React UI Library",
+  "Recoil",
+  "Zustand",
+  "React Query",
+  "Astro",
+  "AWS",
+  'Bun'
 ];
